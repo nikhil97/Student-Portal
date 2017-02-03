be0d912b65b998a07dd781835147c3c6997a5d8e
pragati <shekharpragati143@gmail.com>
2017-02-03 22:43:26 +0530

    UI improved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,24 @@
 
 @section('content')
 
-    <a href = '/myprofile'><button class="btn btn-primary">My Profile</button></a>
 
     <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Search</div>
-
-                <div class="panel-body">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <button id="toggleSearchButton" onclick="toggleSearch()" type="button" class="btn btn-primary">Search
+                </button>
+                <br>
+                <div id="showSearchDiv" class="panel-body" style="display: none;">
                     <form class="form-group" method="get" action={{url('/search')}}>
-                        <input class="form-control" type="text" name="name" placeholder="Name" value="null"><br>
-                        <div class="panel-heading">Branch</div>
+                        <div class="form-group">
+                            <input class="form-control" type="text" name="name" placeholder="Name" ><br>
+                            <input class="form-control" type="text" name="year_of_passing" placeholder="Year Of Passing"><br>
+                        <input class="form-control" type="text" name="skills" placeholder="Skills or Certifiacation"><br>
+                            <input class="form-control" type="text" name="interests" placeholder="Interest"><br></div>
                         <div class="panel-body">
-                            <div class="col-md-6">
-                                <select class = " form-control"  name="branch" >
-                                    <option> </option>
+                            <div class="col-md-6 col-md-offset-6">
+                                <select class=" form-control" name="branch">
+                                    <option value="null">Select Branch</option>
                                     <option>CSE</option>
                                     <option>ME</option>
                                     <option>ECE</option>
@@ -30,10 +32,8 @@
                             </div>
 
                         </div>
-                        <input class="form-control" type="text" name="year_of_passing" placeholder="Year Of Passing"value="null"><br>
-                        <input class="form-control" type="text" name="skills" placeholder="Skills or Certifiacation" value="null"><br>
-                        <input class="form-control" type="text" name="interests" placeholder="Interest"value="null"><br>
-                        <button type="submit" class="btn btn-primary">Search</button>
+
+                        <button type="submit" class="btn btn-primary">GO</button>
                     </form>
 
 
@@ -41,6 +41,16 @@
             </div>
         </div>
     </div>
-</div>
+
+
+    <script type="text/javascript">
+        function toggleSearch() {
+
+            $('#showSearchDiv').delay(200).slideToggle('slow');
+        }
+
+    </script>
+
+
 @endsection
 
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,52 +2,109 @@
 
 @section('content')
 
-<form method="get" action="/user/edit">
-    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <form method="get" action="/user/edit">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <button type=submit class=" btn btn-primary" style="float: right">Edit</button>
 
-    <div class="container">
-        <button type = submit class = " btn btn-primary" style="float: right">Edit</button>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Name</div>
-                    <div class="panel-body">
-                        {{$user->name}}
-
-
-                    </div>
-
-                    <div class="panel-heading">Branch</div>
-                    <div class="panel-body">
-                        {{$user->branch}}
-
-                    </div>
-                    <div class="panel-heading">Year Of Passing</div>
-                    <div class="panel-body">
-                        {{$user->year_of_passing}}
-
-                    </div>
-                    <br>
-                    <div class="panel-heading">Skills</div>
-                    <div class="panel-body">
-                        {{$user->skills}}
-
-                    </div>
-
-                    <div class="panel-heading">Certification</div>
-                         <div class="panel-body">
-                         {{$user->Certication}}</div>
-                    <div class="panel-heading">Training</div>
-                           <div class="panel-body">
-                           {{$user->training}}</div>
-                    <div class="panel-heading">Interest</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Name</div>
                         <div class="panel-body">
-                        {{$user->interests}}</div>
-
+                            {{$user->name}}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</form>
 
-    @endsection
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Branch</div>
+                        <div class="panel-body">
+                            {{$user->branch}}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Year Of Passing</div>
+                        <div class="panel-body">
+                            {{$user->year_of_passing}}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Skills</div>
+                        <div class="panel-body">
+                            {{$user->skills}}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Certification</div>
+                        <div class="panel-body">
+                            {{$user->Certication}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Training</div>
+                        <div class="panel-body">
+                            {{$user->training}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Interest
+                        </div>
+                        <div class="panel-body">
+                            {{$user->interests}}</div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </form>
+
+@endsection
